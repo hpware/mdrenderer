2f718df521cfcd1fd94123f8102450fa87c015ac
Howard <hw@yuanhau.com>
2024-11-26 00:04:20 +0800

    feat: enhance getLink component with URL display and validation improvements

--- src/components/getLink.vue
+++ src/components/getLink.vue
@@ -4,14 +4,18 @@ import { ref } from 'vue';
 const form = ref({ url: '' });
 const host = new URL(location.href).host;
 const url = ref();
+const displayurl = ref();
+const sf1 = ref(false);
 
 function getLink() {
     if (!form.value.url.startsWith("https://") && !form.value.url.startsWith("http://")) {
         alert("請輸入https 或 http 開頭的連結");
         return;
     }
-    url.value = host + "/?u=" + form.value.url;
+    url.value = "https://" + host + "/?u=" + form.value.url;
     navigator.clipboard.writeText(url.value);
+    displayurl.value = url.value;
+    sf1.value = true;
     alert("連結已複製到剪貼簿");
 }
 </script>
@@ -21,6 +25,7 @@ function getLink() {
         <input type="text" v-model="form.url" placeholder="輸入MD檔案的網址" required />
         <button type="submit">創立連結</button>
     </form>
+    <p v-if="sf1">{{ url.value }}</p>
 </template>
 <style scoped>
 input[type='text'] {
