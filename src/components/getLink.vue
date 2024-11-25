4841489e51c48e80a6a44dee114a15d1e49b6c2a
Howard <hw@yuanhau.com>
2024-11-25 13:39:35 +0800

    validate URL input and copy generated link to clipboard

--- src/components/getLink.vue
+++ src/components/getLink.vue
@@ -2,14 +2,23 @@
 import { ref, onMounted } from 'vue';
 
 const form = ref({ url: '' });
+const host = new URL(location.href).host;
+const url = ref();
+
 function getLink() {
-    console.log(form.value.url);
+    if (!form.value.url.includes("https://") || !form..value.url.includes("http://")) {
+        alert("請輸入https 或 http 開頭的連結");
+        return;
+    }
+    url.value = "https://" + host + "/?u=" + form.value.url;
+    navigator.clipboard.writeText(url.value);
+    alert("連結已複製到剪貼簿");
 }
 </script>
 <template>
     <h2>創立連結</h2>
     <form @submit.prevent="getLink">
-        <input type="text" v-model="form.url" placeholder="輸入MD檔案的網址" />
+        <input type="text" v-model="form.url" placeholder="輸入MD檔案的網址" required />
         <button type="submit">創立連結</button>
     </form>
 </template>
